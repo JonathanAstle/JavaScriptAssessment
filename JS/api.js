c8004e6e1fced6c0317721618fcd24ee67125233
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2019-09-06 12:20:34 +0100

    added get

--- JS/api.js
+++ JS/api.js
@@ -1,31 +1,28 @@
-const xhr = new XMLHttpRequest();
+const req = new XMLHttpRequest();
 
 const apiLink = "http://35.240.3.220:9966/petclinic/api/owners";
 
-function link(){
-    xhr.onload = () => {
+    req.onload = () => {
         if (xhr.status ==200) {
-            resolve(xhr.response);
+            console.log("200");
         } 
         else {
             reject("Request Failed");
         }
-
-        if(xhr.getResponseHeader('Content-Type') === 'application/json;charset=UTF-8'){
-            console.log(xhr.responseText);
-        }
+        console.log(req.responseText);
+        //let data = JSON.parse(req.responseText);
     }
-}
 
 function makeRequest(type, link, obj){
-    xhr.open(type, link);
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhr.send(obj);
+    req.open(type, link);
+    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    req.send(obj);
 }
 
 function getData(type, link){
-    xhr.open(type, link);
-    xhr.send();
+    req.open(type, link);
+    req.send();
+    console.log("hitting this");
 }
 
 
